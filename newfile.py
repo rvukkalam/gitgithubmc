bc65d1f1ac130b43173196b0c12c32a7053d27b3
rvukkalam <rajeev.vukkalam@gmail.com>
2023-05-05 13:09:47 -0400

    restoring changes

--- newfile.py
+++ newfile.py
@@ -4,4 +4,12 @@ print(hi)
 
 __new_var__ = 123.456
 
-print(type(
+print(type(__new_var__))
+
+for x in range(1,30,2):
+  print(x)
+  if x > 10:
+    print(x, " is greater than 10")
+
+name = input("Enter your name: ")
+print(name)
