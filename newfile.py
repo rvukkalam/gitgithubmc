eb1ae3d95cf11ea8832e6db117fe34e901cc1b60
rvukkalam <rajeev.vukkalam@gmail.com>
2023-05-05 12:55:03 -0400

    committing new changes

--- newfile.py
+++ newfile.py
@@ -4,12 +4,4 @@ print(hi)
 
 __new_var__ = 123.456
 
-print(type(__new_var__))
-
-for x in range(1,30,2):
-  print(x)
-  if x > 10:
-    print(x, " is greater than 10")
-
-name = input("Enter your name: ")
-print(name)
+print(type(
